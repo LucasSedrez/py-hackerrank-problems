a39f961b94dfeed0bc4efdac286bce8e2cd8844c
Lucas Sedrez <lucassedrez@hotmail.com>
2018-05-13 12:55:07 -0300

    Birthday Chocolate: adding link

--- birthday-chocolate/solution.py
+++ birthday-chocolate/solution.py
@@ -1,3 +1,6 @@
+# LINK : https://www.hackerrank.com/challenges/the-birthday-bar/problem
+
+# SHORT DESCRIPTION:
 # Lily has a chocolate bar that she wants to share it with Ron for his birthday.
 # Each of the squares has an integer on it. She decides to share a contiguous segment of the bar selected
 # such that the length of the segment mathches Ron's birth month and the sum of the integers on
